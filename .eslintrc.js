6fcd8d8ff3e62d7e1b6e7faefcd84e329a85b72d
wsj <wayne.wei@theronts.biz>
2019-03-07 00:25:58 +0800

    update eslint with ts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
-  extends: ["airbnb"],
-  parser: 'babel-eslint',
+  extends: ['airbnb', 'plugin:react/recommended'],
+  parser: '@typescript-eslint/parser',
+  "plugins": ["@typescript-eslint"],
+  parserOptions:  {
+    ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
+    sourceType:  'module',  // Allows for the use of imports
+  },
   env: {
     browser: true,
     node: true,
@@ -9,27 +14,30 @@ module.exports = {
     jest: true,
     jasmine: true,
   },
+  // settings: {
+  //   polyfills: ['fetch', 'promises', 'url'],
+  //   "import/resolver": {
+  //     node: {
+  //       "paths": ["./"],
+  //       "extensions": [
+  //         ".js",
+  //         ".jsx",
+  //         ".ts",
+  //         ".tsx"
+  //       ]
+  //     },
+  //   },
+  //   "import/parsers": {
+  //     "typescript-eslint-parser": [ ".ts", ".tsx" ]
+  //   }
+  // },
   globals: {
     APP_TYPE: true,
   },
   rules: {
-    'max-len': ["error", { "code": 200 }],
-    'react/jsx-filename-extension': [1, { extensions: ['.js'] }],
-    'react/jsx-wrap-multilines': 0,
-    'react/prop-types': 0,
-    'react/forbid-prop-types': 0,
-    'react/jsx-one-expression-per-line': 0,
-    'import/no-unresolved': [2, { ignore: ['^@/', '^umi/'] }],
-    'import/no-extraneous-dependencies': [2, { optionalDependencies: true }],
-    'jsx-a11y/no-noninteractive-element-interactions': 0,
-    'jsx-a11y/click-events-have-key-events': 0,
-    'jsx-a11y/no-static-element-interactions': 0,
-    'jsx-a11y/anchor-is-valid': 0,
-    'jsx-a11y/label-has-associated-control': 0,
-    'linebreak-style': 0,
-    "consistent-return": 0
-  },
-  settings: {
-    polyfills: ['fetch', 'promises', 'url'],
+    // 'react/jsx-filename-extension': [1, { extensions: ['.js'] }],
+    // 'import/no-unresolved': 0
+    // 'import/no-unresolved': [2, { ignore: ['^@/'] }],
+    // 'import/no-extraneous-dependencies': [2, { optionalDependencies: true }],
   },
 }
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,29 +1,29 @@
-const path = require("path");
-const webpack = require("webpack");
-const merge = require("webpack-merge");
-const common = require("./webpack.common");
-const webpackVisualizerPlugin = require("webpack-visualizer-plugin");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const WebpackVisualizerPlugin = require('webpack-visualizer-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const common = require('./webpack.common');
 
 module.exports = merge(common, {
-  mode: "production",
-  devtool: "source-map",
+  mode: 'production',
+  devtool: 'source-map',
   plugins: [
-    new CleanWebpackPlugin(["dist"]),
-    new webpackVisualizerPlugin(),
+    new CleanWebpackPlugin(['dist']),
+    new WebpackVisualizerPlugin(),
     new UglifyJsPlugin({
-      sourceMap: true
+      sourceMap: true,
     }),
     new MiniCssExtractPlugin({
-      filename: "[name].[contenthash].css",
-      chunkFilename: "[id].css"
-    })
+      filename: '[name].[contenthash].css',
+      chunkFilename: '[id].css',
+    }),
   ],
   output: {
-    filename: "index.[chunkhash].js",
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "./"
-  }
+    filename: 'index.[chunkhash].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: './',
+  },
 });
